4a4ac45c313083adcbdaf73c3659217f07a8a518
ernolf <raphael.gradenwitz@googlemail.com>
2026-07-17 14:21:45 +0200

    ci: add psalm static analysis
    Signed-off-by: ernolf <raphael.gradenwitz@googlemail.com>

---
+++ tests/stub.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * SPDX-FileCopyrightText: 2026 [ernolf] Raphael Gradenwitz <raphael.gradenwitz@googlemail.com>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+// Psalm stubs for internal (OC\…) classes that are not part of the public OCP
+// surface provided by nextcloud/ocp. The middleware injection relies on these
+// internals by design; keep the stubs minimal.
+
+namespace {
+	class OC {
+		public static \OCP\IServerContainer $server;
+		public static string $SERVERROOT;
+	}
+}
+
+namespace OC\Http\Client {
+	class Client {
+	}
+}
